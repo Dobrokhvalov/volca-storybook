aa1effaa2e4c8dff6b413b26dd9aa9f1939710a4
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2019-02-12 15:36:56 +0300

    Pending Claim Page implemented

--- src/components/pages/ClaimPendingPage.jsx
+++ src/components/pages/ClaimPendingPage.jsx
@@ -7,10 +7,29 @@ import PendingIcon from '../common/PendingIcon';
 import './pages.css';
 
 
+
 const PageContent = () => {
+    const etherscanLink = "#";
     return (
 	<div className="claim-pending-page">
 	  <PendingIcon/>
+          <div className="text-center">
+            <div className="title">
+              Claiming...
+            </div>
+            <div className="subtitle">
+              Transaction is processing
+            </div>
+            <div className="text">
+              It may take a few minutes. You can<br />
+	      check status later in 'Wallet'.
+            </div>
+            <div>
+              <div className="text">
+                Details on <a className="link" href={etherscanLink}>Etherscan</a>
+              </div>
+            </div>
+          </div>	  
 	</div>
     );
 }
