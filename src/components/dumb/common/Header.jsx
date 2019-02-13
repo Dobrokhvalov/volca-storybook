1c6e5bcac8723487c2ff30f2a583cefcc33e03cb
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2019-02-14 02:27:45 +0300

    dada logo added

--- src/components/dumb/common/Header.jsx
+++ src/components/dumb/common/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import RetinaImage from 'react-retina-image'
 import './common.css'
 
 class Header extends React.Component {
   render () {
     return (
       <div className='header'>
-        Get tokens
+        <RetinaImage
+          src={`https://raw.githubusercontent.com/VolcaTech/eth2-assets/master/images/dada-logo.png`} />
       </div>
     )
   }
