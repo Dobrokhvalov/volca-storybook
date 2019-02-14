eb95db04d70a86efc75799f7b63aff8b5dedb5d9
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2019-02-14 03:34:57 +0300

    intro screen added

--- src/components/NotConnectedScreens/NoWalletScreen.jsx
+++ src/components/NotConnectedScreens/NoWalletScreen.jsx
@@ -4,17 +4,33 @@ import { getDeviceOS } from './../../utils'
 import { setupPortisWeb3 } from '../../actions/web3'
 import NoWalletPageDesktop from './../dumb/pages/NoWalletPageDesktop'
 import NoWalletPageMobile from './../dumb/pages/NoWalletPageMobile'
+import IntroPage from './../dumb/pages/ClaimPage'
 const qs = require('querystring')
 
-const NoWalletPage = ({ setupPortisWeb3, location }) => {
-  if (window.innerWidth < 769) {
-    // parse url params
-    const queryParams = qs.parse(location.search.substring(1))
-    const walletId = (queryParams.wallet || queryParams.w)
-    const os = getDeviceOS()
-    return <NoWalletPageMobile deviceOS={os} walletId={walletId} showPortisModal={setupPortisWeb3} />
+class NoWalletPage extends React.Component {
+  state = {
+    page: 'intro'
   }
-  return <NoWalletPageDesktop showPortisModal={setupPortisWeb3} />
+  
+  _renderRedirectPage () {
+    if (window.innerWidth < 769) {
+      // parse url params
+      const queryParams = qs.parse(this.props.location.search.substring(1))
+      const walletId = (queryParams.wallet || queryParams.w)
+      const os = getDeviceOS()
+      return <NoWalletPageMobile deviceOS={os} walletId={walletId} showPortisModal={this.props.setupPortisWeb3} />
+    }
+    return <NoWalletPageDesktop showPortisModal={this.props.setupPortisWeb3} />
+  }
+
+  render () {
+    if (this.state.page === 'intro') {
+      return <IntroPage onSubmit={() => this.setState({ page: 'redirect' })} /> 
+    } 
+    return this._renderRedirectPage()
+  }
 }
+
+
 
 export default connect(null, { setupPortisWeb3 })(NoWalletPage)
