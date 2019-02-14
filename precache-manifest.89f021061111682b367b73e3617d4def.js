915bf6ddade97b3e8ab45ffa6fbccd0b8b1f8909
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2019-02-14 03:40:12 +0300

    Updates

---
+++ precache-manifest.89f021061111682b367b73e3617d4def.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2329a9a6885668f4f7f7",
+    "url": "/static/js/main.2329a9a6.chunk.js"
+  },
+  {
+    "revision": "02908fd5bf43452f8b75",
+    "url": "/static/js/1.02908fd5.chunk.js"
+  },
+  {
+    "revision": "2329a9a6885668f4f7f7",
+    "url": "/static/css/main.0bee6ada.chunk.css"
+  },
+  {
+    "revision": "b840bff79a4933206eddb24c8281e61e",
+    "url": "/index.html"
+  }
+];
